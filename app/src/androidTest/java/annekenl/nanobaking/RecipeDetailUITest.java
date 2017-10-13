7429230bc7e282a2785fb8a9dbfa336a7cffdbce
lanakin <lanneken@gmail.com>
2017-10-13 11:46:44 -0400

    a couple more espresso UI tests for the navigation buttons

--- app/src/androidTest/java/annekenl/nanobaking/RecipeDetailUITest.java
+++ app/src/androidTest/java/annekenl/nanobaking/RecipeDetailUITest.java
@@ -1,11 +1,10 @@
 package annekenl.nanobaking;
 
 import android.content.Intent;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,12 +19,14 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static annekenl.nanobaking.RecipeDetailNavFragment.RECIPE_ITEM_OBJ;
+import static annekenl.nanobaking.TestUtilities.getARecipeItem;
 
 /**
- *  Model after example test in Udacity Lesson Project - 'Tea Time'
+ *  Reference Information: example test in Udacity Lesson Project - 'Tea Time'
  * and stack overflow question: https://stackoverflow.com/questions/31752303/espresso-startactivity-that-depends-on-intent
  */
 
@@ -35,199 +36,6 @@ public class RecipeDetailUITest
 {
     private RecipeItem mTestRecipeObj = new RecipeItem();
 
-    private String testJSONStr = "[\n" +
-            "  {\n" +
-            "    \"id\": 1,\n" +
-            "    \"name\": \"Nutella Pie\",\n" +
-            "    \"ingredients\": [\n" +
-            "      {\n" +
-            "        \"quantity\": 2,\n" +
-            "        \"measure\": \"CUP\",\n" +
-            "        \"ingredient\": \"Graham Cracker crumbs\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 6,\n" +
-            "        \"measure\": \"TBLSP\",\n" +
-            "        \"ingredient\": \"unsalted butter, melted\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 0.5,\n" +
-            "        \"measure\": \"CUP\",\n" +
-            "        \"ingredient\": \"granulated sugar\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 1.5,\n" +
-            "        \"measure\": \"TSP\",\n" +
-            "        \"ingredient\": \"salt\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 5,\n" +
-            "        \"measure\": \"TBLSP\",\n" +
-            "        \"ingredient\": \"vanilla\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 1,\n" +
-            "        \"measure\": \"K\",\n" +
-            "        \"ingredient\": \"Nutella or other chocolate-hazelnut spread\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 500,\n" +
-            "        \"measure\": \"G\",\n" +
-            "        \"ingredient\": \"Mascapone Cheese(room temperature)\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 1,\n" +
-            "        \"measure\": \"CUP\",\n" +
-            "        \"ingredient\": \"heavy cream(cold)\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"quantity\": 4,\n" +
-            "        \"measure\": \"OZ\",\n" +
-            "        \"ingredient\": \"cream cheese(softened)\"\n" +
-            "      }\n" +
-            "    ],\n" +
-            "    \"steps\": [\n" +
-            "      {\n" +
-            "        \"id\": 0,\n" +
-            "        \"shortDescription\": \"Recipe Introduction\",\n" +
-            "        \"description\": \"Recipe Introduction\",\n" +
-            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4\",\n" +
-            "        \"thumbnailURL\": \"\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"id\": 1,\n" +
-            "        \"shortDescription\": \"Starting prep\",\n" +
-            "        \"description\": \"1. Preheat the oven to 350\\u00b0F. Butter a 9\\\" deep dish pie pan.\",\n" +
-            "        \"videoURL\": \"\",\n" +
-            "        \"thumbnailURL\": \"\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"id\": 2,\n" +
-            "        \"shortDescription\": \"Prep the cookie crust.\",\n" +
-            "        \"description\": \"2. Whisk the graham cracker crumbs, 50 grams (1/4 cup) of sugar, and 1/2 teaspoon of salt together in a medium bowl. Pour the melted butter and 1 teaspoon of vanilla into the dry ingredients and stir together until evenly mixed.\",\n" +
-            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9a6_2-mix-sugar-crackers-creampie/2-mix-sugar-crackers-creampie.mp4\",\n" +
-            "        \"thumbnailURL\": \"\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"id\": 3,\n" +
-            "        \"shortDescription\": \"Press the crust into baking form.\",\n" +
-            "        \"description\": \"3. Press the cookie crumb mixture into the prepared pie pan and bake for 12 minutes. Let crust cool to room temperature.\",\n" +
-            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9cb_4-press-crumbs-in-pie-plate-creampie/4-press-crumbs-in-pie-plate-creampie.mp4\",\n" +
-            "        \"thumbnailURL\": \"\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"id\": 4,\n" +
-            "        \"shortDescription\": \"Start filling prep\",\n" +
-            "        \"description\": \"4. Beat together the nutella, mascarpone, 1 teaspoon of salt, and 1 tablespoon of vanilla on medium speed in a stand mixer or high speed with a hand mixer until fluffy.\",\n" +
-            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd97a_1-mix-marscapone-nutella-creampie/1-mix-marscapone-nutella-creampie.mp4\",\n" +
-            "        \"thumbnailURL\": \"\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"id\": 5,\n" +
-            "        \"shortDescription\": \"Finish filling prep\",\n" +
-            "        \"description\": \"5. Beat the cream cheese and 50 grams (1/4 cup) of sugar on medium speed in a stand mixer or high speed with a hand mixer for 3 minutes. Decrease the speed to medium-low and gradually add in the cold cream. Add in 2 teaspoons of vanilla and beat until stiff peaks form.\",\n" +
-            "        \"videoURL\": \"\",\n" +
-            "        \"thumbnailURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffda20_7-add-cream-mix-creampie/7-add-cream-mix-creampie.mp4\"\n" +
-            "      },\n" +
-            "      {\n" +
-            "        \"id\": 6,\n" +
-            "        \"shortDescription\": \"Finishing Steps\",\n" +
-            "        \"description\": \"6. Pour the filling into the prepared crust and smooth the top. Spread the whipped cream over the filling. Refrigerate the pie for at least 2 hours. Then it's ready to serve!\",\n" +
-            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffda45_9-add-mixed-nutella-to-crust-creampie/9-add-mixed-nutella-to-crust-creampie.mp4\",\n" +
-            "        \"thumbnailURL\": \"\"\n" +
-            "      }\n" +
-            "    ],\n" +
-            "    \"servings\": 8,\n" +
-            "    \"image\": \"\"\n" +
-            "  }]";
-
-
-    //for test purposes returns after it completes creating
-    //one test RecipeItem.
-    protected RecipeItem parseRecipeJson(String json)
-    {
-        RecipeItem emptyRecipeItem = new RecipeItem();
-
-        try
-        {
-            JSONArray recipes = new JSONArray(json);
-
-            for(int i = 0; i < recipes.length(); i++)
-            {
-                JSONObject currRecipe = recipes.getJSONObject(i);
-                RecipeItem recipeItem = new RecipeItem();
-
-                if(currRecipe.has("id"))
-                    recipeItem.setId(currRecipe.getInt("id"));
-                if(currRecipe.has("name"))
-                    recipeItem.setName(currRecipe.getString("name"));
-
-                if(currRecipe.has("ingredients"))
-                {       //create ingredients list
-                    JSONArray ingredients = currRecipe.getJSONArray("ingredients");
-                    ArrayList<IngredientItem> ingredientItems = new ArrayList<>();
-
-                    for(int j = 0; j < ingredients.length(); j++)
-                    {
-                        JSONObject currIngredient = ingredients.getJSONObject(j);
-                        IngredientItem ingredientItem = new IngredientItem();
-
-                        if(currIngredient.has("quantity"))
-                            ingredientItem.setQuantity(currIngredient.getString("quantity"));
-                        if(currIngredient.has("measure"))
-                            ingredientItem.setMeasure(currIngredient.getString("measure"));
-                        if(currIngredient.has("ingredient"))
-                            ingredientItem.setIngredient(currIngredient.getString("ingredient")); //name
-
-                        ingredientItems.add(ingredientItem);
-                    }
-
-                    recipeItem.setIngredients(ingredientItems);
-                }
-
-                if(currRecipe.has("steps"))
-                {       //create steps list
-                    JSONArray steps = currRecipe.getJSONArray("steps");
-                    ArrayList<StepItem> stepItems = new ArrayList<>();
-
-                    for(int k = 0; k < steps.length(); k++)
-                    {
-                        JSONObject currStep = steps.getJSONObject(k);
-                        StepItem stepItem = new StepItem();
-
-                        if(currStep.has("id"))
-                            stepItem.setId(currStep.getInt("id"));
-                        if(currStep.has("shortDescription"))
-                            stepItem.setShortDesc(currStep.getString("shortDescription"));
-                        if(currStep.has("description"))
-                            stepItem.setDescription(currStep.getString("description"));
-                        if(currStep.has("videoURL"))
-                            stepItem.setVideoUrl(currStep.getString("videoURL"));
-                        if(currStep.has("thumbnailURL"))
-                            stepItem.setThumbnailUrl(currStep.getString("thumbnailURL"));
-
-                        stepItems.add(stepItem);
-                    }
-
-                    recipeItem.setSteps(stepItems);
-                }
-
-                if(currRecipe.has("servings"))
-                    recipeItem.setServings(currRecipe.getString("servings"));
-                if(currRecipe.has("image"))
-                    recipeItem.setImageUrl(currRecipe.getString("image"));
-
-                return recipeItem;
-            }
-
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return emptyRecipeItem;
-    }
-
     @Rule
     public ActivityTestRule<RecipeDetailActivity> mActivityRule =
             new ActivityTestRule<RecipeDetailActivity>(RecipeDetailActivity.class){
@@ -236,7 +44,7 @@ public class RecipeDetailUITest
                 protected Intent getActivityIntent() {
 
                     /* create the test Recipe item */
-                    mTestRecipeObj = parseRecipeJson(testJSONStr);
+                    mTestRecipeObj = getARecipeItem();
 
                     Intent intent = new Intent();
                     intent.putExtra(RECIPE_ITEM_OBJ, mTestRecipeObj);
@@ -292,6 +100,98 @@ public class RecipeDetailUITest
         onView(withText(aStepItem.getShortDesc() + "\n\n" +
                 aStepItem.getDescription() + "\n")).check(matches(isDisplayed())); //recipe details text
 
+    }
+
+
+    /** Navigation Check **/
+
+    /**
+     * The first "recipe sub part" shown in details for a single recipe is the ingredients.
+     * The only available navigation button is the next button to proceed to the first step.
+     * (the system back button will go back to the overall button list of recipe sub parts)
+     */
+    @Test
+    public void testFirstRecipePartNav()
+    {
+        onView(withText(R.string.detail_ingredients_button)).perform(click());
+
+        onView(withId(R.id.nextBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.prevBtn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+
+        onView(withId(R.id.nextBtn)).perform(click());
+
+        StepItem aStepItem = mTestRecipeObj.getSteps().get(0);
+
+        onView(withText(aStepItem.getShortDesc() + "\n\n" +
+                aStepItem.getDescription() + "\n")).check(matches(isDisplayed())); //showing the next part (step 0)
+    }
+
+
+    /**
+     * The last "recipe sub part" shown in details for a single recipe is the last recipe step.
+     * The only available navigation button is the previous button to go back to the second-to-last step.
+     * (the system back button will go back to the overall button list of recipe sub parts)
+     */
+    @Test
+    public void testLastRecipePartNav()
+    {
+        int stepsSize = mTestRecipeObj.getSteps().size();
+        StepItem aStepItem = mTestRecipeObj.getSteps().get(stepsSize-1);
+
+        onView(withText("Step " + aStepItem.getId() + " - " + aStepItem.getShortDesc())).perform(click());
+
+        onView(withId(R.id.prevBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.nextBtn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+
+        onView(withId(R.id.prevBtn)).perform(click());
+
+        aStepItem = mTestRecipeObj.getSteps().get(stepsSize-2);
+
+        onView(withText(aStepItem.getShortDesc() + "\n\n" +
+                aStepItem.getDescription() + "\n")).check(matches(isDisplayed())); //showing the second-to-last step
+    }
+
+
+    /**
+     * In-between the ingredients list and the last recipe step - a middle recipe step has a previous and
+     * next button for going back and forward among the steps in order.
+     * (the system back button will go back to the overall button list of recipe sub parts)
+     */
+    @Test
+    public void testMiddleRecipePartNav()
+    {
+        int stepsSize = mTestRecipeObj.getSteps().size();
+        StepItem aStepItem = mTestRecipeObj.getSteps().get(1); //starting at step 1
+
+        onView(withText("Step " + aStepItem.getId() + " - " + aStepItem.getShortDesc())).perform(click());
+
+        onView(withId(R.id.prevBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.nextBtn)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.prevBtn)).perform(click());  //MOVE BACK from step 1 to step 0
+
+        aStepItem = mTestRecipeObj.getSteps().get(0);
+
+        onView(withText(aStepItem.getShortDesc() + "\n\n" +
+                aStepItem.getDescription() + "\n")).check(matches(isDisplayed())); //confirm we're at step 0
+
+       //click next button at step 0 to go to step 1
+        onView(withId(R.id.nextBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.nextBtn)).perform(click());    //GO FORWARD to step 1
+
+        aStepItem = mTestRecipeObj.getSteps().get(1);
+
+        onView(withText(aStepItem.getShortDesc() + "\n\n" +
+                aStepItem.getDescription() + "\n")).check(matches(isDisplayed())); //confirm we're at step 1
+
+        //click next button at step 1 to go to step 2
+        onView(withId(R.id.nextBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.nextBtn)).perform(click());    //GO FORWARD again to step 2
+
+        aStepItem = mTestRecipeObj.getSteps().get(2);
+
+        onView(withText(aStepItem.getShortDesc() + "\n\n" +
+                aStepItem.getDescription() + "\n")).check(matches(isDisplayed())); //confirm we're at step 2
     }
 
 }
---
+++ app/src/androidTest/java/annekenl/nanobaking/TestUtilities.java
@@ -0,0 +1,226 @@
+package annekenl.nanobaking;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import annekenl.nanobaking.recipedata.IngredientItem;
+import annekenl.nanobaking.recipedata.RecipeItem;
+import annekenl.nanobaking.recipedata.StepItem;
+
+/**
+ * Created by annekenl1 on 10/13/17.
+ */
+
+public class TestUtilities
+{
+    public static ArrayList<RecipeItem> mRecipes = new ArrayList<RecipeItem>();
+
+    /* test JSON with a single Recipe */
+    public static final String testJSONStr = "[\n" +
+            "  {\n" +
+            "    \"id\": 1,\n" +
+            "    \"name\": \"Nutella Pie\",\n" +
+            "    \"ingredients\": [\n" +
+            "      {\n" +
+            "        \"quantity\": 2,\n" +
+            "        \"measure\": \"CUP\",\n" +
+            "        \"ingredient\": \"Graham Cracker crumbs\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 6,\n" +
+            "        \"measure\": \"TBLSP\",\n" +
+            "        \"ingredient\": \"unsalted butter, melted\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 0.5,\n" +
+            "        \"measure\": \"CUP\",\n" +
+            "        \"ingredient\": \"granulated sugar\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 1.5,\n" +
+            "        \"measure\": \"TSP\",\n" +
+            "        \"ingredient\": \"salt\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 5,\n" +
+            "        \"measure\": \"TBLSP\",\n" +
+            "        \"ingredient\": \"vanilla\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 1,\n" +
+            "        \"measure\": \"K\",\n" +
+            "        \"ingredient\": \"Nutella or other chocolate-hazelnut spread\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 500,\n" +
+            "        \"measure\": \"G\",\n" +
+            "        \"ingredient\": \"Mascapone Cheese(room temperature)\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 1,\n" +
+            "        \"measure\": \"CUP\",\n" +
+            "        \"ingredient\": \"heavy cream(cold)\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"quantity\": 4,\n" +
+            "        \"measure\": \"OZ\",\n" +
+            "        \"ingredient\": \"cream cheese(softened)\"\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"steps\": [\n" +
+            "      {\n" +
+            "        \"id\": 0,\n" +
+            "        \"shortDescription\": \"Recipe Introduction\",\n" +
+            "        \"description\": \"Recipe Introduction\",\n" +
+            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd974_-intro-creampie/-intro-creampie.mp4\",\n" +
+            "        \"thumbnailURL\": \"\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 1,\n" +
+            "        \"shortDescription\": \"Starting prep\",\n" +
+            "        \"description\": \"1. Preheat the oven to 350\\u00b0F. Butter a 9\\\" deep dish pie pan.\",\n" +
+            "        \"videoURL\": \"\",\n" +
+            "        \"thumbnailURL\": \"\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 2,\n" +
+            "        \"shortDescription\": \"Prep the cookie crust.\",\n" +
+            "        \"description\": \"2. Whisk the graham cracker crumbs, 50 grams (1/4 cup) of sugar, and 1/2 teaspoon of salt together in a medium bowl. Pour the melted butter and 1 teaspoon of vanilla into the dry ingredients and stir together until evenly mixed.\",\n" +
+            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9a6_2-mix-sugar-crackers-creampie/2-mix-sugar-crackers-creampie.mp4\",\n" +
+            "        \"thumbnailURL\": \"\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 3,\n" +
+            "        \"shortDescription\": \"Press the crust into baking form.\",\n" +
+            "        \"description\": \"3. Press the cookie crumb mixture into the prepared pie pan and bake for 12 minutes. Let crust cool to room temperature.\",\n" +
+            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd9cb_4-press-crumbs-in-pie-plate-creampie/4-press-crumbs-in-pie-plate-creampie.mp4\",\n" +
+            "        \"thumbnailURL\": \"\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 4,\n" +
+            "        \"shortDescription\": \"Start filling prep\",\n" +
+            "        \"description\": \"4. Beat together the nutella, mascarpone, 1 teaspoon of salt, and 1 tablespoon of vanilla on medium speed in a stand mixer or high speed with a hand mixer until fluffy.\",\n" +
+            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffd97a_1-mix-marscapone-nutella-creampie/1-mix-marscapone-nutella-creampie.mp4\",\n" +
+            "        \"thumbnailURL\": \"\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 5,\n" +
+            "        \"shortDescription\": \"Finish filling prep\",\n" +
+            "        \"description\": \"5. Beat the cream cheese and 50 grams (1/4 cup) of sugar on medium speed in a stand mixer or high speed with a hand mixer for 3 minutes. Decrease the speed to medium-low and gradually add in the cold cream. Add in 2 teaspoons of vanilla and beat until stiff peaks form.\",\n" +
+            "        \"videoURL\": \"\",\n" +
+            "        \"thumbnailURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffda20_7-add-cream-mix-creampie/7-add-cream-mix-creampie.mp4\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"id\": 6,\n" +
+            "        \"shortDescription\": \"Finishing Steps\",\n" +
+            "        \"description\": \"6. Pour the filling into the prepared crust and smooth the top. Spread the whipped cream over the filling. Refrigerate the pie for at least 2 hours. Then it's ready to serve!\",\n" +
+            "        \"videoURL\": \"https://d17h27t6h515a5.cloudfront.net/topher/2017/April/58ffda45_9-add-mixed-nutella-to-crust-creampie/9-add-mixed-nutella-to-crust-creampie.mp4\",\n" +
+            "        \"thumbnailURL\": \"\"\n" +
+            "      }\n" +
+            "    ],\n" +
+            "    \"servings\": 8,\n" +
+            "    \"image\": \"\"\n" +
+            "  }]";
+
+
+    public static RecipeItem parseRecipeJson(String json)
+    {
+        RecipeItem emptyRecipeItem = new RecipeItem();
+
+        try
+        {
+            JSONArray recipes = new JSONArray(json);
+
+            for(int i = 0; i < recipes.length(); i++)
+            {
+                JSONObject currRecipe = recipes.getJSONObject(i);
+                RecipeItem recipeItem = new RecipeItem();
+
+                if(currRecipe.has("id"))
+                    recipeItem.setId(currRecipe.getInt("id"));
+                if(currRecipe.has("name"))
+                    recipeItem.setName(currRecipe.getString("name"));
+
+                if(currRecipe.has("ingredients"))
+                {       //create ingredients list
+                    JSONArray ingredients = currRecipe.getJSONArray("ingredients");
+                    ArrayList<IngredientItem> ingredientItems = new ArrayList<>();
+
+                    for(int j = 0; j < ingredients.length(); j++)
+                    {
+                        JSONObject currIngredient = ingredients.getJSONObject(j);
+                        IngredientItem ingredientItem = new IngredientItem();
+
+                        if(currIngredient.has("quantity"))
+                            ingredientItem.setQuantity(currIngredient.getString("quantity"));
+                        if(currIngredient.has("measure"))
+                            ingredientItem.setMeasure(currIngredient.getString("measure"));
+                        if(currIngredient.has("ingredient"))
+                            ingredientItem.setIngredient(currIngredient.getString("ingredient")); //name
+
+                        ingredientItems.add(ingredientItem);
+                    }
+
+                    recipeItem.setIngredients(ingredientItems);
+                }
+
+                if(currRecipe.has("steps"))
+                {       //create steps list
+                    JSONArray steps = currRecipe.getJSONArray("steps");
+                    ArrayList<StepItem> stepItems = new ArrayList<>();
+
+                    for(int k = 0; k < steps.length(); k++)
+                    {
+                        JSONObject currStep = steps.getJSONObject(k);
+                        StepItem stepItem = new StepItem();
+
+                        if(currStep.has("id"))
+                            stepItem.setId(currStep.getInt("id"));
+                        if(currStep.has("shortDescription"))
+                            stepItem.setShortDesc(currStep.getString("shortDescription"));
+                        if(currStep.has("description"))
+                            stepItem.setDescription(currStep.getString("description"));
+                        if(currStep.has("videoURL"))
+                            stepItem.setVideoUrl(currStep.getString("videoURL"));
+                        if(currStep.has("thumbnailURL"))
+                            stepItem.setThumbnailUrl(currStep.getString("thumbnailURL"));
+
+                        stepItems.add(stepItem);
+                    }
+
+                    recipeItem.setSteps(stepItems);
+                }
+
+                if(currRecipe.has("servings"))
+                    recipeItem.setServings(currRecipe.getString("servings"));
+                if(currRecipe.has("image"))
+                    recipeItem.setImageUrl(currRecipe.getString("image"));
+
+                mRecipes.add(recipeItem);  //main recipes list
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return emptyRecipeItem;
+    }
+
+
+    /**
+     * Common helper method to return a test Recipe object
+     * created from JSON data
+     *
+     * @return  a single RecipeItem
+     */
+    public static RecipeItem getARecipeItem()
+    {
+        parseRecipeJson(testJSONStr);
+
+        return mRecipes.get(0);
+    }
+
+    /* to do - lengthen test json string to create more recipe items */
+}
